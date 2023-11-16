039e8fd3b9c17b469cb184edf8cfcf8f32ee7110
Documenter.jl <documenter@juliadocs.github.io>
2023-11-16 12:00:48 +0000

    build based on 5f74177

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaML","category":"page"},{"location":"#JuliaML","page":"Home","title":"JuliaML","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaML.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaML]","category":"page"}]
+}
